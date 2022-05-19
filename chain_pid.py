cbe7ade560e7286d7e9d2753afda81e7435a8370
Dmitry Mingazov <mingazov@email.com>
2022-05-19 14:26:59 +0200

    Add grouping chains by class name

--- chain_pid.py
+++ chain_pid.py
@@ -1,15 +1,20 @@
 from file_rw import read_chains_from_file
-from filters import filter_chains_smaller_than
+from filters import filter_chains_bigger_than, filter_chains_smaller_than, group_by_class_name
 
 def print_chain_stats(chains):
     print(f"Size of filtered chains: {len(chains)}")
+def print_grouped_chain_stats(grouped_chains):
+    for cn in grouped_chains:
+        print(f"{cn}: {len(grouped_chains[cn])} chains")
 
 def main():
     input_file = "../logs/primi30krecord.csv"
-    output_file = "../logs/chains/rsu_chain"
+    # output_file = "../logs/chains/rsu_chain"
     chains = read_chains_from_file(input_file)
-    filtered_chains = filter_chains_smaller_than(chains, 2)
-    print_chain_stats(filtered_chains)
+    filtered_chains = filter_chains_bigger_than(chains, 3)
+    grouped_chains = group_by_class_name(filtered_chains)
+    print_grouped_chain_stats(grouped_chains)
+    # print_chain_stats(filtered_chains)
 
 if __name__ == "__main__":
     main()
--- filters.py
+++ filters.py
@@ -12,3 +12,27 @@ def filter_chains_by_len(chains, threshold, is_less_then):
         if len_lesser_threshold == is_less_then:
             filtered_chains.append(chain)
     return filtered_chains
+
+def get_chain_cn(chain):
+    occurences = {}
+    max = -1
+    max_class_name = ""
+    for row in chain:
+        class_name = row["cn"][:3]
+        occ = occurences.get(class_name, 0) + 1
+        occurences[class_name] = occ 
+        if occ > max:
+            max = occ
+            max_class_name = class_name
+    return max_class_name
+
+def group_by_class_name(chains):
+    grouped_chains = {}
+    for chain in chains:
+        cn = get_chain_cn(chain)
+        group = grouped_chains.get(cn, [])
+        group.append(chain)
+        grouped_chains[cn] = group
+    return grouped_chains
+    # for chain in chains:
+
