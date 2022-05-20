7658494e8c19ccd7374f3232331cbbfe1ca216ce
Dmitry Mingazov <mingazov@email.com>
2022-05-20 15:57:49 +0200

    Add writing chains on file between given indexes

--- file_rw.py
+++ file_rw.py
@@ -14,7 +14,7 @@ def write_group_chain(chains, directory, cn):
         if e.errno != errno.EEXIST:
             raise
     file_prefix = cn_dir + cn + "chain"
-    write_chain_on_file(chains, file_prefix)
+    write_chains_on_file(chains, file_prefix)
 
 def write_feature_file(headers, features):
     filename = "feature_file.csv"
@@ -25,19 +25,29 @@ def write_feature_file(headers, features):
             writer.writerow(row.values())
         f.close()
 
-def write_chain_on_file(chains, file_prefix):
-    headers = chains[0][0].keys()
+def write_chain_on_file(chain, filename):
+    headers = chain[0].keys()
+    with open(filename, 'w', encoding="utf-8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        for row in chain:
+            writer.writerow(row.values())
+        f.close()
+
+def write_chains_on_file_from_to(chains, file_prefix, start, end):
+    file_index = start
+    for chain in chains[start:end]:
+        filename = file_prefix + str(file_index) + ".csv"
+        file_index += 1
+        write_chain_on_file(chain, filename)
+
+def write_chains_on_file(chains, file_prefix):
     file_index = 1
     print(f"Writing {len(chains)} on file")
     for chain in chains:
         filename = file_prefix + str(file_index) + ".csv"
         file_index += 1
-        with open(filename, 'w', encoding="utf-8", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow(headers)
-            for row in chain:
-                writer.writerow(row.values())
-            f.close()
+        write_chain_on_file(chain, filename)
 
 def add_id(id, chain_index, valuable_ids):
     if (id not in valuable_ids) and id:
@@ -52,13 +62,13 @@ def init_new_chain(id1, id2, chains_size, valuable_ids):
     return chains_size
 
 
-def read_chains_from_file(filename):
+def read_chains_from_file(filename, delimiter):
     chains_size = -1
     chains = []
     valuable_ids = {}
 
     with open(filename, encoding="utf-8") as f:
-        csv_reader = csv.DictReader(f, delimiter=";")
+        csv_reader = csv.DictReader(f, delimiter=delimiter)
         line_count = 0
         for row in csv_reader:
             # init the first chain
