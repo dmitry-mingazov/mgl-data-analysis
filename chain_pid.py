71f39a14124c6f980bf85ee22506f10f7ae4d862
Dmitry Mingazov <mingazov@email.com>
2022-05-20 16:15:38 +0200

    Add print group cids statistics

--- chain_pid.py
+++ chain_pid.py
@@ -1,22 +1,43 @@
 # from clustering import create_feature_file
-from file_rw import read_chains_from_file, write_group_chain
+from file_rw import read_chains_from_file, write_chains_on_file_from_to, write_group_chain
 from filters import *
 
 def print_chain_stats(chains):
     print(f"Size of filtered chains: {len(chains)}")
+
 def print_grouped_chain_stats(grouped_chains):
     for cn in grouped_chains:
         print(f"{cn}: {len(grouped_chains[cn])} chains")
 
+def print_group_cids(chains):
+    occurences = {}
+    for chain in chains:
+        cid = chain[0]["cid"]
+        occ = occurences.get(cid, 0) + 1
+        occurences[cid] = occ
+    for cid in occurences:
+        print(f"{cid}: {occurences[cid]} chains")
+
+
 def main():
-    input_file = "../logs/primi30krecord.csv"
+    input_file = "../logs/100k_raw.csv"
     output_dir = "../logs/chains/"
-    chains = read_chains_from_file(input_file)
+    reports_dir = "../reports/"
+    delimiter = ","
+    cn = "ProFRMAllegati"
+    chains = read_chains_from_file(input_file, delimiter)
     # filtered_chains = filter_chains_bigger_than(delete_duplicate_rows(chains), 3)
-    filtered_chains = filter_chains_bigger_than(chains, 3)
-    grouped_chains = filter_grouped_chains_smaller_than(group_by_first_class(filtered_chains), 3)
+    fc = filter_chains_bigger_than(chains, 3)
+    fc2 = delete_duplicate_rows(fc)
+    gc = group_by_first_class(fc2)
+    print_group_cids(gc[cn])
+    # print_grouped_chain_stats(gc)
+    # write_group_chain(gc[cn], output_dir, cn)
+    # print_chain_stats(fc)
+    # write_chains_on_file_from_to(filtered_chains, output_dir, 795, 847)
+    # grouped_chains = filter_grouped_chains_smaller_than(group_by_first_class(filtered_chains), 3)
     # create_feature_file(filtered_chains)
-    print_grouped_chain_stats(grouped_chains)
+    # print_grouped_chain_stats(grouped_chains)
     # grouped_chains = filter_grouped_chains_smaller_than(group_by_class_name(filtered_chains), 8)
     # print(f"Groups: {len(grouped_chains)}")
     # print_grouped_chain_stats(grouped_chains)
