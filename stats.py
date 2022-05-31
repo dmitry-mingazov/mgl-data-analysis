08d0e2618a08e2c14e35a95fe8680a766bbb32ff
Dmitry Mingazov <mingazov@email.com>
2022-05-31 10:23:13 +0200

    Add last row statistics

--- stats.py
+++ stats.py
@@ -5,3 +5,12 @@ def stats_first_row(chains, column):
         occ = occurences.get(value, 0) + 1
         occurences[value] = occ
     return occurences
+
+def stats_last_row(chains, column):
+    occurences = {}
+    for chain in chains:
+        value = chain[-1][column]
+        occ = occurences.get(value, 0) + 1
+        occurences[value] = occ
+    return occurences
+
